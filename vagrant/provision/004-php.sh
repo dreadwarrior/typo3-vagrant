5b2e44ebe24cff4506f39fb0f1aaeee15585c0ae
Tommy Juhnke <tommy@van-tomas.de>
2014-02-24 22:19:18 +0100

    [TASK] splitting php install/config; reording provision scripts

--- vagrant/provision/004-php.sh
+++ vagrant/provision/004-php.sh
@@ -2,7 +2,7 @@
 
 echo "Installing PHP..."
 
-apt-get -y -qq install php5-fpm php5 php5-cli php5-curl php5-gd php5-json php5-mcrypt php5-mysql php5-sqlite php5-suhosin php5-xdebug php5-xsl
+apt-get -y -qq install php5-fpm php5 php5-cli php5-curl php5-gd php5-json php5-mcrypt php5-mysql php5-sqlite php5-xdebug php5-xsl
 
 service php5-fpm restart
 service nginx restart
--- vagrant/provision/005-php55.sh
+++ vagrant/provision/005-php55.sh
@@ -6,16 +6,3 @@ echo "Updating PHP to version 5.5..."
 apt-get -y -qq install python-software-properties
 add-apt-repository -y ppa:ondrej/php5
 apt-get -y -qq update && apt-get -y -qq dist-upgrade
-
-# opcache adjustments, see http://forge.typo3.org/issues/51475
-ln -s /vagrant/vagrant/config/php5-fpm/conf.d/06-opcache.ini /etc/php5/fpm/conf.d/06-opcache.ini
-# additional xdebug settings
-ln -s /vagrant/vagrant/config/php5-fpm/conf.d/21-xdebug.ini /etc/php5/fpm/conf.d/21-xdebug.ini
-
-# removal. of default pool
-mv /etc/php5/fpm/pool.d/www.conf /etc/php5/fpm/pool.d/www.conf.bak
-# adding our own pool
-ln -s /vagrant/vagrant/config/php5-fpm/pool.d/vagrant.conf /etc/php5/fpm/pool.d/vagrant.conf
-
-service php5-fpm restart
-service nginx restart
